1d09c2fe33eee74fa82e0b3d7c96a2c8c23baabf
Qusai <qusai2002h@gmail.com>
2024-10-05 16:38:59 +0300

    Added userlist and fixed sidenavbar and added mainligunpage

---
+++ Mozaic.PasswordManager.Web/Controllers/UsersListController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore; // Ensure you have this for EF Core async methods
+using Mozaic.PasswordManager.Web.Models.DBEntities;
+using Mozaic.PasswordManager.Web.Models.ViewModels;
+using System.Linq; // Ensure you have this for LINQ queries
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Mozaic.PasswordManager.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class SystemUserController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SystemUserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Action to display all users
+        public async Task<IActionResult> Users()
+        {
+            var users = await _context.SystemUsers
+                .Select(u => new SystemUserViewModel
+                {
+                    Id = u.Id,
+                    UserName = u.UserName,
+                    IsAdmin = u.IsAdmin ?? false
+                })
+                .ToListAsync();
+
+            return View("/Views/Admin/Users.cshtml", users);
+        }
+
+        // Action to display edit user form
+        public async Task<IActionResult> Edit(int id)
+        {
+            var user = await _context.SystemUsers.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new SystemUserViewModel
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                IsAdmin = user.IsAdmin ?? false // Assign false if IsAdmin is null
+            };
+
+            return View(viewModel);
+        }
+
+        // POST action for saving edited user data
+        [HttpPost]
+        public async Task<IActionResult> Edit(SystemUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Log validation errors
+                var errors = ModelState.Values.SelectMany(v => v.Errors);
+                foreach (var error in errors)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+                return View(model); // Return to the same view with the model to show validation errors
+            }
+
+            var user = await _context.SystemUsers.FindAsync(model.Id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.UserName = model.UserName;
+            user.IsAdmin = model.IsAdmin;
+
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Users)); // Redirect to the Users action
+        }
+
+        // Action for changing password
+        public async Task<IActionResult> ChangePassword(int id)
+        {
+            var user = await _context.SystemUsers.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new SystemUserViewModel
+            {
+                Id = user.Id,
+                UserName = user.UserName
+            };
+
+            return View(viewModel);
+        }
+
+        // POST action for changing password
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(int id, string newPassword)
+        {
+            var user = await _context.SystemUsers.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            // Hash the new password (using BCrypt for password hashing)
+            user.HashedPassword = BCrypt.Net.BCrypt.HashPassword(newPassword); // Ensure you update the correct property for the hashed password
+
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Users)); // Redirect to the Users action
+        }
+    }
+}
--- Mozaic.PasswordManager.Web/wwwroot/js/scripts.js
+++ Mozaic.PasswordManager.Web/wwwroot/js/scripts.js
@@ -1,11 +1,10 @@
 /*!
-    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2023 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    // 
+ * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+ */
+
 // Scripts
-// 
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -13,14 +12,14 @@ window.addEventListener('DOMContentLoaded', event => {
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
         // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
+        if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+            document.body.classList.toggle('sb-sidenav-toggled');
+        }
+
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
     }
-
 });
